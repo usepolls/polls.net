2b25aea9a14d6008eee3997222aa6299be9c8fcf
Charlie Joseph <ch@rli.es>
2022-11-20 19:02:26 +0000

    Update wording

--- pages/index.vue
+++ pages/index.vue
@@ -15,7 +15,7 @@
         <h1 class="mt-2 text-4xl md:text-5xl font-semibold text-brand-dark tracking-wide !leading-tight">
           The Open-Source Player Feedback Platform
         </h1>
-        <p class="mt-4 text-2xl mx-auto !leading-snug lg:w-9/12 text-gray-500">Ask your players for feedback, and gain per-poll analytics and understand your community reaction.</p>
+        <p class="mt-4 text-2xl mx-auto !leading-snug lg:w-9/12 text-gray-500">{{ description }}</p>
         <div class="mt-6">
           <a href="https://twitter.com/heychazza" class="text-base text-sky-600 group md:text-lg">Follow us <span class="group-hover:underline">@heychazza</span> on Twitter<span aria-hidden="true"> →</span></a>
         </div>
@@ -27,7 +27,7 @@
 
 <script setup>
 const title = 'Polls.net';
-const description = 'Ask your players for feedback, and gain per-poll analytics and understand your community reaction.';
+const description = 'Polls is a lightweight Minecraft plugin that allows server admins to easily create and manage in-game polls.';
 
 useHead({
   title: title,
